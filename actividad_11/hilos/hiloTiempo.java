d5529a99fed04db19eaff69dc84f68a1c696b6e0
PabloPizarro8 <ppizarro.c32@hotmail.com>
2022-03-21 22:20:15 -0700

    Create hiloTiempo.java

---
+++ actividad_11/hilos/hiloTiempo.java
@@ -0,0 +1,14 @@
+/*
+
+Esta clase sera donde el hilo sera ejecutado
+ */
+package hilos;
+
+public class hiloTiempo extends Thread{
+    String tiempoString = "";
+    
+    @Override
+    public void run(){
+        
+    }
+}
